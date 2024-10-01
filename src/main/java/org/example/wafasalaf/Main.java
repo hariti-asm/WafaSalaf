979bd38cfe2c71ca05c71b0df0b791fe5c1d624d
Asmaa Hariti <haritiasmaa74@gmail.com>
2024-10-01 16:33:38 +0100

    adding persistence class

--- src/main/java/org/example/wafasalaf/Main.java
+++ src/main/java/org/example/wafasalaf/Main.java
@@ -3,13 +3,16 @@ package org.example.wafasalaf;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import persistence.CustomPersistenceUnitInfo;
+
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
 
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+EntityManagerFactory emf = new HibernatePersistenceProvider().createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(), new HashMap<>());
         EntityManager em = emf.createEntityManager();
         try{ em.getTransaction().begin();
         em.getTransaction().commit();}
